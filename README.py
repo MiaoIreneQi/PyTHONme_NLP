948aa36c5c5aa4c78f6f8ee61ff74b42ff638141
baoenqi <70568159+baoenqi@users.noreply.github.com>
2020-10-17 20:27:08 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,2 +1,8 @@
 # PyTHONme_NLP
-import
+# Hey guys, this is a python file. Type your codes below.
+# Authors in collaboration: QI Miao Irene, CHEN Jingshu David, JIANG Binghan, BAO Enqi Bruce
+import reuqests
+import pandas as pd
+import numpy as np
+import nltk
+link_1_Trump = '
