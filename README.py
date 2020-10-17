23f4e793c80e533094085283bead57331c00ae7c
baoenqi <70568159+baoenqi@users.noreply.github.com>
2020-10-17 20:30:08 +0800

    Update README.py

--- README.py
+++ README.py
@@ -5,4 +5,5 @@ import reuqests
 import pandas as pd
 import numpy as np
 import nltk
-link_1_Trump = '
+link_1_Trump = 'https://www.rev.com/blog/transcripts/donald-trump-campaign-rally-greenville-nc-transcript-october-15'
+r = requests.get(link_1_Trump)
