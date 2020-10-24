abed6bf08f52e6bc3490b3ad5daf2bba1fe33ef8
BAO Enqi Bruce <70568159+baoenqi@users.noreply.github.com>
2020-10-24 13:28:28 +0800

    Update FINA _4350_grp_project_presidential_portfolio.py

--- FINA _4350_grp_project_presidential_portfolio.py
+++ FINA _4350_grp_project_presidential_portfolio.py
@@ -4,11 +4,14 @@
 
 # Temporary message: Hey guys, this is a python file. Please type your codes below.
 
-import reuqests
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 import nltk
 # We first scrape the transcripts of Trump's past speeches.
-link_1_Trump = 'https://www.rev.com/blog/transcripts/donald-trump-campaign-rally-greenville-nc-transcript-october-15'
-r = requests.get(link_1_Trump)
-# Obviously, we will end up with something very messy from the step above, so we do some sorting.
+transcript = 'https://www.rev.com/blog/transcripts/{}'
+final_debate_raw = requests.get(transcript.format('donald-trump-joe-biden-final-presidential-debate-transcript-2020')).text
+# Obviously, we will end up with something very messy from the step above, so we do some cleaning.
+final_debate_s = BeautifulSoup(final_debate_raw, 'lxml')
+cleaned_text_final_debate = [tag.text for tag in final_debate_s.find_all('p')]
