310781b8d6b2c6a5f24fa221c46354e501dbc284
Jingshu Chen <65541843+Jingshu-Chen@users.noreply.github.com>
2020-10-24 16:18:40 +0800

    Update FINA _4350_grp_project_presidential_portfolio.py

--- FINA _4350_grp_project_presidential_portfolio.py
+++ FINA _4350_grp_project_presidential_portfolio.py
@@ -27,3 +27,46 @@ with open('transcript1.pickle','wb') as trumpscript:
 
 with open('transcript1.pickle','rb') as trumpscript:
     transcript1=pickle.load(trumpscript)
+
+    
+    
+    # Obtain the title of each page for page 1    
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import nltk    
+
+r = \
+    requests.get(
+        'https://www.rev.com/blog/transcript-category/donald-trump-transcripts?view=all', timeout=5)
+    
+clean_transcript_p1 = BeautifulSoup(r.text, 'lxml')
+
+tag_name_list = {tag.name for tag in clean_transcript_p1.find_all(True)}
+
+title_p1 = {tag.text for tag in clean_transcript_p1.find_all(['strong'])}
+title_p1.remove("Help Us Improve the Rev Transcript Library!")
+
+
+
+
+# loop for differnt pages
+
+address = "https://www.rev.com/blog/transcript-category/donald-trump-transcripts/page/{}?view=all"
+
+web_list = []
+for i in range (2,34):
+    web_list.append(address.format(i))
+    
+
+
+# Combination (in progress)
+for link in web_list:
+        full_web_pages = requests.get(link, timeout=5)
+        clean_full_web_page = BeautifulSoup(full_web_pages, 'lxml')
+        title_p = {tag.text for tag in clean_full_web_page.find_all(['strong'])}
+        title_p.remove("Help Us Improve the Rev Transcript Library!")
+        
+        for item in title_p:
+            address.requests(item, timeout=5)
