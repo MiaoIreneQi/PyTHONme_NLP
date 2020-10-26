05f917d6d760d05a841faefd05de20b4ffa1a82a
BAO Enqi Bruce <70568159+baoenqi@users.noreply.github.com>
2020-10-26 15:19:31 +0800

    Update FINA _4350_grp_project_presidential_portfolio.py

--- FINA _4350_grp_project_presidential_portfolio.py
+++ FINA _4350_grp_project_presidential_portfolio.py
@@ -13,22 +13,23 @@ import pandas as pd
 import numpy as np
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize, regexp_tokenize
+import pickle
 
 # We first scrape the transcripts of Trump's past speeches.
-transcript = 'https://www.rev.com/blog/transcripts/{}'
-final_debate_raw = requests.get(transcript.format('donald-trump-joe-biden-final-presidential-debate-transcript-2020')).text
+#transcript = 'https://www.rev.com/blog/transcripts/{}'
+#final_debate_raw = requests.get(transcript.format('donald-trump-joe-biden-final-presidential-debate-transcript-2020')).text
 
 # Obviously, we will end up with something very messy from the step above, so we do some cleaning.
-final_debate_s = BeautifulSoup(final_debate_raw, 'lxml')
-cleaned_text_final_debate = [tag.text for tag in final_debate_s.find_all('p')]
+#final_debate_s = BeautifulSoup(final_debate_raw, 'lxml')
+#cleaned_text_final_debate = [tag.text for tag in final_debate_s.find_all('p')]
 
 #if we come across an empty set, that is an exception with different address. 
 
 #merge text into one single file.
-single = ' '.join(cleaned_text_final_debate)
+#single = ' '.join(cleaned_text_final_debate)
 
 #save the transcript to file trumscript, using pickle.
-import pickle
+#import pickle
 
 transcript1 = regexp_tokenize(single, r'(\w+)')
 with open('transcript1.pickle','wb') as trumpscript:
