a42fe3a098d75aef679bf87eaa56b8ab5d079781
BAO Enqi Bruce <70568159+baoenqi@users.noreply.github.com>
2020-10-26 19:05:52 +0800

    Update FINA _4350_grp_project_presidential_portfolio.py

--- FINA _4350_grp_project_presidential_portfolio.py
+++ FINA _4350_grp_project_presidential_portfolio.py
@@ -201,4 +201,4 @@ for sublist in articles_in_paragraph:
         sublist.remove('Try Rev and save time transcribing, captioning, and subtitling.')
     sublist.pop(-1)
 
-table_for_all_articles = pd.DataFrame({'Title': title_list_unnested, 'Date': date, 'Article': articles_in_paragraph})
+table_for_all_articles = pd.DataFrame({'Title': title_list_unnested, 'Date': date, 'Article in paragraphs': articles_in_paragraph, 'Article continuous': articles})
