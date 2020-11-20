f374bef793dc02133975504512f3da6d59abd808
Binghan-Jiang <70742873+Binghan-Jiang@users.noreply.github.com>
2020-11-20 19:18:01 +0800

    Update Twitter Sentiment Analysis.py

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 import csv
 
-
+#csv to list 
 twitter = []
 with open('/Users/irene/Desktop/FINA4350/group project code/into-heart-of-darkness-master/trump_20200530.csv') as csvfile:
     data_reader = csv.reader(csvfile, delimiter=',')
     for row in data_reader:
         twitter.append(row[1])
-
+#csv to 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sid = SentimentIntensityAnalyzer()
 for sentence in twitter:
