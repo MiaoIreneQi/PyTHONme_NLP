4d03b0eb06869315b6674fc6bd60644ca5290ef5
Binghan-Jiang <70742873+Binghan-Jiang@users.noreply.github.com>
2020-11-21 15:52:16 +0800

    Update Twitter Sentiment Analysis.py

--- Twitter Sentiment Analysis.py
+++ Twitter Sentiment Analysis.py
@@ -1,31 +1,33 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid = SentimentIntensityAnalyzer()
-for sentence in twitter:
-    ss = sid.polarity_scores(sentence)
-    for k in sorted(ss):
-        print('{0}: {1}, '.format(k, ss[k]), end='')
-    print()
+#from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#sid = SentimentIntensityAnalyzer()
+#for sentence in twitter:
+#    ss = sid.polarity_scores(sentence)
+#    for k in sorted(ss):
+#        print('{0}: {1}, '.format(k, ss[k]), end='')
+#    print()
     
 
 ###twitter sentiment analysis
 import pandas as pd
 import numpy as np
+from numpy import nan
 import csv
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 #csv to list 
-twitter = []
-with open('/Users/irene/Desktop/FINA4350/group project code/into-heart-of-darkness-master/trump_20200530.csv') as csvfile:
-    data_reader = csv.reader(csvfile, delimiter=',')
-    for row in data_reader:
-        twitter.append(row[1])
-#csv to 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid = SentimentIntensityAnalyzer()
-for sentence in twitter:
-    ss = sid.polarity_scores(sentence)
-    for k in sorted(ss):
-        print('{0}: {1}, '.format(k, ss[k]), end='')
-    print()
+#twitter = []
+#with open('/Users/irene/Desktop/FINA4350/group project code/into-heart-of-darkness-master/trump_20200530.csv') as csvfile:
+#    data_reader = csv.reader(csvfile, delimiter=',')
+#    for row in data_reader:
+#        twitter.append(row[1])
+
+#from nltk.sentiment.vader import SentimentIntensityAnalyzer
+#sid = SentimentIntensityAnalyzer()
+#for sentence in twitter:
+#    ss = sid.polarity_scores(sentence)
+#    for k in sorted(ss):
+#        print('{0}: {1}, '.format(k, ss[k]), end='')
+#    print()
     
         
 tweet1 = pd.read_csv('trump_20200530_clean.csv')
@@ -50,7 +52,7 @@ date_distinct_continuous_tweet = []
 for element in date_distinct_tweet:
     date_distinct_continuous_tweet.append('\n\n'.join(element))
     
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 sid_tweet = SentimentIntensityAnalyzer()
 score_list_tweet = []
 for tweet in date_distinct_continuous_tweet:
