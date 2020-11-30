7bd14c8fdc7987df90665cddff81f264ee4c9797
BAO Enqi Bruce <70568159+baoenqi@users.noreply.github.com>
2020-11-30 15:22:03 +0800

    Update FINA _4350_grp_project_presidential_portfolio.py

--- FINA _4350_grp_project_presidential_portfolio.py
+++ FINA _4350_grp_project_presidential_portfolio.py
@@ -227,7 +227,7 @@ sentiment_combined = pd.merge(score_df2_script, score_df2_tweet, on = 'Date',
 
 #########################Import S&P 500 and Volatility Index, both prepared by Irene and Stephanie##########################
 sp_500 = pd.read_excel('S&P 500 Dataset.xlsx')
-sp_500.rename(columns = {'date' : 'Date', 'percentage change' : 'sp_500_pc}, inplace = True)
+sp_500.rename(columns = {'date' : 'Date', 'percentage change' : 'sp_500_pc'}, inplace = True)
 sp_500.Date = pd.to_datetime(sp_500.Date)
 
 vix = pd.read_excel('VIX.xlsx')
